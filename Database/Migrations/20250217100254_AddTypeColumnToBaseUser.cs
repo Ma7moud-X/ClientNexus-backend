7a16741ce93fc3b32aa4c86f089c2fe04a7eec23
Mahmoud Abuelnaga <mahmoud.abuelnaga0@gmail.com>
2025-02-17 12:03:46 +0200

    Adding UserType column to BaseUser

---
+++ Database/Migrations/20250217100254_AddTypeColumnToBaseUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypeColumnToBaseUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserType",
+                table: "BaseUsers",
+                type: "varchar(1)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "BaseUsers");
+        }
+    }
+}
--- Database/Models/Users/BaseUser.cs
+++ Database/Models/Users/BaseUser.cs
@@ -2,6 +2,13 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Database.Models.Users;
 
+public enum UserType
+{
+    Client = 'C',
+    ServiceProvider = 'S',
+    Admin = 'A',
+}
+
 public class BaseUser : IdentityUser<int>
 {
     public string FirstName { get; set; } = default!;
@@ -9,7 +16,7 @@ public class BaseUser : IdentityUser<int>
     public bool IsBlocked { get; set; } = false;
     public bool IsDeleted { get; set; } = false;
     public DateOnly BirthDate { get; set; }
-
+    public char UserType { get; set; }
 
     public int? BlockedById { get; set; }
     public Admin? BlockedBy { get; set; }
