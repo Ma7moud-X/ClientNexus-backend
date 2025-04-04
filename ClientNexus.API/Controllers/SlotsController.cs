d5691a3ee1d71db4557653a79c75d5bdd31827dc
Jasmine Mamdouh <112454290+JasmineMamdouh@users.noreply.github.com>
2025-04-04 17:37:13 +0200

    edit slot add and update endpoints

--- ClientNexus.API/Controllers/SlotsController.cs
+++ ClientNexus.API/Controllers/SlotsController.cs
@@ -4,6 +4,7 @@ using ClientNexus.Application.Services;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using ClientNexus.Application.DTO;
 using ClientNexus.Application.Interfaces;
+using ClientNexus.Domain.Enums;
 
 namespace ClientNexus.API.Controllers
 {
@@ -23,13 +24,14 @@ namespace ClientNexus.API.Controllers
         /// Get available slots for a service provider within a date range
         /// </summary>
         [HttpGet("available")]
-        public async Task<ActionResult<IEnumerable<SlotDTO>>> GetAvailableSlots(
+        public async Task<ActionResult<IEnumerable<SlotDTO>>> GetSlots(
             int serviceProviderId,
             DateTime startDate,
-            DateTime endDate)
+            DateTime endDate,
+            SlotType type,
+            SlotStatus? status)
         {
-            var slots = await _slotService.GetAvailableSlotsAsync(serviceProviderId, startDate, endDate);
-            return Ok(slots);
+            return Ok(await _slotService.GetSlotsAsync(serviceProviderId, startDate, endDate, type, status));
         }
 
         /// <summary>
@@ -38,13 +40,14 @@ namespace ClientNexus.API.Controllers
         [HttpGet("{id:int}", Name = "GetSlotById")]
         public async Task<ActionResult<SlotDTO>> GetSlotById(int id)
         {
-            var slot = await _slotService.GetSlotByIdAsync(id);
-            return Ok(slot);
+            return Ok(await _slotService.GetSlotByIdAsync(id));
         }
 
         // <summary>
         /// Create a new slot
         /// </summary>
+        /// 
+        //authorize: admin , provider
         [HttpPost]
         public async Task<ActionResult<SlotDTO>> CreateSlot([FromBody] SlotCreateDTO slotDTO)
         {
@@ -53,18 +56,31 @@ namespace ClientNexus.API.Controllers
         }
 
         // <summary>
-        /// Update aslot
+        /// Update a slot
         /// </summary>
+        /// 
+        //authorize: admin, provider
         [HttpPut("{id:int}")]
-        public async Task<ActionResult<SlotDTO>> UpdateSlot(int id, [FromBody] SlotDTO slotDTO)
+        public async Task<IActionResult> UpdateSlot(int id, [FromBody] SlotDTO slotDTO)
         {
-            var slot = await _slotService.Update(id, slotDTO);
-            return NoContent();
+            return Ok(await _slotService.Update(id, slotDTO));
         }
 
+        // <summary>
+        /// Update specific slot status
+        /// </summary>
+        
+        //authorize: admin, provider, client 'to cancel'
+        [HttpPatch("{id:int}/status")]
+        public async Task<IActionResult> UpdateSlotStatus(int id, [FromBody] SlotStatus status)
+        {
+            return Ok( await _slotService.UpdateStatus(id, status));
+        }
         /// <summary>
         /// Delete slot
         /// </summary>
+         
+        //authorize: admin
         [HttpDelete("{id:int}", Name = "DeleteSlot")]
         public async Task<IActionResult> DeleteSlot(int id)
         {
--- ClientNexus.Application/Services/SlotService.cs
+++ ClientNexus.Application/Services/SlotService.cs
@@ -25,24 +25,33 @@ namespace ClientNexus.Application.Services
         }
 
         //pagination by date range for calendar display
-        public async Task<IEnumerable<SlotDTO>> GetAvailableSlotsAsync(int serviceProviderId, DateTime startDate, DateTime endDate)
+        public async Task<IEnumerable<SlotDTO>> GetSlotsAsync(int serviceProviderId, DateTime startDate, DateTime endDate, SlotType type, SlotStatus? status)
         {
             if (startDate > endDate)
                 throw new ArgumentException("Start date must be before end date.");
-            //no past slots
-            /*
-            if (startDate < DateTime.Now)
-                throw new ArgumentException("Start date cannot be in the past");
-            */
+            
             var providerExists = await _unitOfWork.ServiceProviders.GetByIdAsync(serviceProviderId);
             if (providerExists == null)
                 throw new ArgumentException("Invalid Service Provider ID.");
 
+            // Validate that the passed enum values
+            if (!Enum.IsDefined(type))
+                throw new ArgumentException($"Invalid SlotType value: {type}");
+
+            status ??= SlotStatus.Available;
+
+            if (!Enum.IsDefined(status.Value))
+                throw new ArgumentException($"Invalid SlotStatus value: {status}");
+
+            if (startDate < DateTime.Now && status == SlotStatus.Available)
+                startDate = DateTime.Now;  //don't retrieve past avaliable slots
+
             IEnumerable<Slot> slots = await _unitOfWork.Slots.GetByConditionAsync(s => 
-                s.ServiceProviderId == serviceProviderId && 
-                s.Status == SlotStatus.Available && 
+                s.ServiceProviderId == serviceProviderId &&  
                 s.Date >= startDate && 
-                s.Date <= endDate);
+                s.Date <= endDate &&
+                s.SlotType == type &&
+                s.Status == status);
 
             return _mapper.Map<List<SlotDTO>>(slots);
         }
@@ -50,27 +59,21 @@ namespace ClientNexus.Application.Services
         {
             Slot? slot = await _unitOfWork.Slots.FirstOrDefaultAsync(s => s.Id == slotId); 
             if (slot == null)
-            {
                 throw new KeyNotFoundException("Invalid ID");
-            }
+
             return _mapper.Map<SlotDTO>(slot);
         }
         public async Task<SlotDTO> CreateAsync([FromBody] SlotCreateDTO slotDTO)
         {
             if (slotDTO == null)
-            {
                 throw new ArgumentNullException("Slot data cannot be null");
-            }
             //check if foreign key is valid
             if(await _unitOfWork.ServiceProviders.GetByIdAsync(slotDTO.ServiceProviderId) == null)
-            {
                 throw new ArgumentException("Invalid Service Provider Id");
-            }
             //check if it is a valid date
             if (slotDTO.Date < DateTime.UtcNow)
-            {
                 throw new ArgumentException("Slot date cannot be in the past");
-            }
+
             Slot slot = _mapper.Map<Slot>(slotDTO);
             slot.Status = SlotStatus.Available;
             var createdSlot = await _unitOfWork.Slots.AddAsync(slot);
@@ -90,10 +93,7 @@ namespace ClientNexus.Application.Services
             //check if updated foreign key is valid
             if (await _unitOfWork.ServiceProviders.GetByIdAsync(slotDTO.ServiceProviderId) == null)
                  throw new ArgumentException("Invalid Service Provider Id");
-            //check if updated date is a valid date
-            if (slotDTO.Date < DateTime.UtcNow)
-                throw new ArgumentException("Slot date cannot be in the past");
-
+   
             Slot updatedSlot = _mapper.Map<Slot>(slotDTO);
             updatedSlot = _unitOfWork.Slots.Update(existingSlot, updatedSlot);
             await _unitOfWork.SaveChangesAsync();
@@ -101,13 +101,23 @@ namespace ClientNexus.Application.Services
 
         }
 
+        public async Task<SlotDTO> UpdateStatus(int id, SlotStatus status)
+        {
+            var slot = await _unitOfWork.Slots.GetByIdAsync(id);
+            if (slot == null)
+                throw new KeyNotFoundException("Slot not found.");
+
+            slot.Status = status;
+            await _unitOfWork.SaveChangesAsync();
+            return _mapper.Map<SlotDTO>(slot);
+        }
+
         public async Task DeleteAsync (int slotId)
         {
             Slot? slot = await _unitOfWork.Slots.FirstOrDefaultAsync(s => s.Id == slotId);
             if (slot == null)
-            {
-                throw new KeyNotFoundException("Invalid slot ID");
-            }
+                 throw new KeyNotFoundException("Invalid slot ID");
+
             _unitOfWork.Slots.Delete(slot);
             await _unitOfWork.SaveChangesAsync();
 
