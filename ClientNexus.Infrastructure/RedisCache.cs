0794b7bc1b5db292fac89880a6ce19445a6b69ed
Mahmoud Abuelnaga <mahmoud.abuelnaga0@gmail.com>
2025-04-09 19:13:49 +0200

    Custom Exceptions for infrastructure layer

--- ClientNexus.Infrastructure/RedisCache.cs
+++ ClientNexus.Infrastructure/RedisCache.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using ClientNexus.Domain.Enums;
+using ClientNexus.Domain.Exceptions.ServerErrorsExceptions;
 using ClientNexus.Domain.Interfaces;
 using ClientNexus.Domain.ValueObjects;
 using StackExchange.Redis;
@@ -52,23 +53,27 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.GeoAddAsync(key, longitude, latitude, identifier);
+
+        bool added;
+        try
+        {
+            added = await executor.GeoAddAsync(key, longitude, latitude, identifier);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error adding geo location: `${identifier}` to key: `{key}`",
+                ex
+            );
+        }
+
+        return added;
     }
 
     public async Task<long> AddToListObjectAsync<T>(string key, T value)
     {
-        if (string.IsNullOrWhiteSpace(key))
-        {
-            throw new ArgumentException("key cannot be null or whitespace", nameof(key));
-        }
-
-        if (value is null)
-        {
-            throw new ArgumentException("value cannot be null", nameof(value));
-        }
-
-        IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.ListRightPushAsync(key, JsonSerializer.Serialize(value));
+        ArgumentNullException.ThrowIfNull(value);
+        return await AddToListStringAsync(key, JsonSerializer.Serialize(value));
     }
 
     public async Task<long> AddToListStringAsync(string key, string value)
@@ -84,7 +89,18 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.ListRightPushAsync(key, value);
+
+        long pushedCount;
+        try
+        {
+            pushedCount = await executor.ListRightPushAsync(key, value);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error pushing value to list: `${value}`", ex);
+        }
+
+        return pushedCount;
     }
 
     public async Task<bool> CommitTransactionAsync()
@@ -94,7 +110,15 @@ public class RedisCache : ICache
             throw new InvalidOperationException("No transaction is currently active.");
         }
 
-        bool result = await _transaction.ExecuteAsync();
+        bool result;
+        try
+        {
+            result = await _transaction.ExecuteAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException("Error committing transaction", ex);
+        }
         _transaction = null;
 
         return result;
@@ -108,7 +132,17 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        RedisValue[] res = await executor.ListRangeAsync(key);
+
+        RedisValue[] res;
+        try
+        {
+            res = await executor.ListRangeAsync(key);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error getting list from key: `${key}`", ex);
+        }
+
         return res.Length == 0
             ? null
             : res.Select(v => JsonSerializer.Deserialize<T>(v.ToString()));
@@ -122,19 +156,23 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        RedisValue[] res = await executor.ListRangeAsync(key);
+
+        RedisValue[] res;
+        try
+        {
+            res = await executor.ListRangeAsync(key);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error getting list from key: `${key}`", ex);
+        }
+
         return res.Length == 0 ? null : res.Select(v => v.ToString());
     }
 
     public async Task<T?> GetObjectAsync<T>(string key)
     {
-        if (string.IsNullOrWhiteSpace(key))
-        {
-            throw new ArgumentException("key cannot be null or whitespace", nameof(key));
-        }
-
-        IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        string? res = await executor.StringGetAsync(key);
+        string? res = await GetStringAsync(key);
         return res is null ? default : JsonSerializer.Deserialize<T>(res);
     }
 
@@ -146,7 +184,18 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.StringGetAsync(key);
+
+        RedisValue res;
+        try
+        {
+            res = await executor.StringGetAsync(key);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error getting string from key: `${key}`", ex);
+        }
+
+        return res;
     }
 
     public async Task<bool> RemoveGeoLocationAsync(string key, string identifier)
@@ -165,7 +214,21 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.GeoRemoveAsync(key, identifier);
+
+        bool removed;
+        try
+        {
+            removed = await executor.GeoRemoveAsync(key, identifier);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error removing geo location: `${identifier}` from key: `${key}`",
+                ex
+            );
+        }
+
+        return removed;
     }
 
     public async Task<bool> RemoveKeyAsync(string key)
@@ -176,7 +239,18 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.KeyDeleteAsync(key);
+
+        bool keyRemoved;
+        try
+        {
+            keyRemoved = await executor.KeyDeleteAsync(key);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error removing key: `${key}`", ex);
+        }
+
+        return keyRemoved;
     }
 
     public async Task<string?> GetListStringAsync(string key, int index)
@@ -186,35 +260,28 @@ public class RedisCache : ICache
             throw new ArgumentException("key cannot be null or whitespace", nameof(key));
         }
 
-        // if (index < 0)
-        // {
-        //     throw new ArgumentException("index cannot be less than zero", nameof(index));
-        // }
-
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.ListGetByIndexAsync(key, index);
+
+        RedisValue res;
+        try
+        {
+            res = await executor.ListGetByIndexAsync(key, index);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error getting list item at index: `${index}` from key: `${key}`",
+                ex
+            );
+        }
+
+        return res;
     }
 
     public async Task<T?> GetListObjectAsync<T>(string key, int index)
     {
-        if (string.IsNullOrWhiteSpace(key))
-        {
-            throw new ArgumentException("key cannot be null or whitespace", nameof(key));
-        }
-
-        // if (index < 0)
-        // {
-        //     throw new ArgumentException("index cannot be less than zero", nameof(index));
-        // }
-
-        IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        var res = await executor.ListGetByIndexAsync(key, index);
-        if (res.IsNull)
-        {
-            return default;
-        }
-
-        return JsonSerializer.Deserialize<T>(res.ToString());
+        var res = await GetListStringAsync(key, index);
+        return res is null ? default : JsonSerializer.Deserialize<T>(res);
     }
 
     public async Task<bool> SetExpiryAsync(string key, TimeSpan expiration)
@@ -233,23 +300,24 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.KeyExpireAsync(key, expiration);
+
+        bool expirySet;
+        try
+        {
+            expirySet = await executor.KeyExpireAsync(key, expiration);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException($"Error setting expiry for key: `${key}`", ex);
+        }
+
+        return expirySet;
     }
 
     public async Task<bool> SetObjectAsync<T>(string key, T value, TimeSpan? expiration = null)
     {
-        if (string.IsNullOrWhiteSpace(key))
-        {
-            throw new ArgumentException("key cannot be null or whitespace", nameof(key));
-        }
-
-        if (value is null)
-        {
-            throw new ArgumentException("value cannot be null", nameof(value));
-        }
-
-        IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.StringSetAsync(key, JsonSerializer.Serialize(value), expiration);
+        ArgumentNullException.ThrowIfNull(value);
+        return await SetStringAsync(key, JsonSerializer.Serialize(value), expiration);
     }
 
     public async Task<bool> SetStringAsync(string key, string value, TimeSpan? expiration = null)
@@ -270,7 +338,21 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.StringSetAsync(key, value, expiration);
+
+        bool isSet;
+        try
+        {
+            isSet = await executor.StringSetAsync(key, value, expiration);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error setting string value: `${value}` to key: `${key}`",
+                ex
+            );
+        }
+
+        return isSet;
     }
 
     public bool StartTransaction()
@@ -297,7 +379,21 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.SetAddAsync(key, value);
+
+        bool added;
+        try
+        {
+            added = await executor.SetAddAsync(key, value);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error adding value: `${value}` to set with key: `${key}`",
+                ex
+            );
+        }
+
+        return added;
     }
 
     public async Task<bool> RemoveFromSetStringAsync(string key, string value)
@@ -313,7 +409,21 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.SetRemoveAsync(key, value);
+
+        bool removed;
+        try
+        {
+            removed = await executor.SetRemoveAsync(key, value);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error removing value: `${value}` from set with key: `${key}`",
+                ex
+            );
+        }
+
+        return removed;
     }
 
     public async Task<bool> CheckExistsInSetAsync(string key, string value)
@@ -329,7 +439,21 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        return await executor.SetContainsAsync(key, value);
+
+        bool exists;
+        try
+        {
+            exists = await executor.SetContainsAsync(key, value);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error checking existence of value: `${value}` in set with key: `${key}`",
+                ex
+            );
+        }
+
+        return exists;
     }
 
     public async Task<IEnumerable<RelativeLocation>?> GetGeoLocationsInRadiusAsync(
@@ -372,14 +496,27 @@ public class RedisCache : ICache
         };
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        var res = await executor.GeoRadiusAsync(
-            key,
-            longitude,
-            latitude,
-            radius,
-            geoUnit,
-            options: GeoRadiusOptions.WithCoordinates | GeoRadiusOptions.WithDistance
-        );
+
+        GeoRadiusResult[] res;
+        try
+        {
+            res = await executor.GeoRadiusAsync(
+                key,
+                longitude,
+                latitude,
+                radius,
+                geoUnit,
+                options: GeoRadiusOptions.WithCoordinates | GeoRadiusOptions.WithDistance
+            );
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error getting geolocation within radius: `${radius}` from (longitude: `${longitude}`, latitude: `${latitude}`) from key: `${key}`",
+                ex
+            );
+        }
+
         if (res.Length == 0)
         {
             return null;
@@ -428,13 +565,25 @@ public class RedisCache : ICache
         };
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        var res = await executor.GeoRadiusAsync(
-            key,
-            identifier,
-            radius,
-            geoUnit,
-            options: GeoRadiusOptions.WithCoordinates | GeoRadiusOptions.WithDistance
-        );
+
+        GeoRadiusResult[] res;
+        try
+        {
+            res = await executor.GeoRadiusAsync(
+                key,
+                identifier,
+                radius,
+                geoUnit,
+                options: GeoRadiusOptions.WithCoordinates | GeoRadiusOptions.WithDistance
+            );
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error getting geolocation within radius: `${radius}` from identifier: `${identifier}` from key: `${key}`",
+                ex
+            );
+        }
 
         if (res.Length == 0)
         {
@@ -466,7 +615,20 @@ public class RedisCache : ICache
         }
 
         IDatabaseAsync executor = _transaction is not null ? _transaction : _database;
-        var res = await executor.GeoPositionAsync(key, identifier);
+
+        GeoPosition? res;
+        try
+        {
+            res = await executor.GeoPositionAsync(key, identifier);
+        }
+        catch (Exception ex)
+        {
+            throw new CacheException(
+                $"Error getting geolocation with identifier: `${identifier}` from key: `${key}`",
+                ex
+            );
+        }
+
         return res is null
             ? null
             : new Location
