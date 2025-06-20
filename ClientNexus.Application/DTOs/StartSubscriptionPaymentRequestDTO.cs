571513c7b4e22a98fb4317464a0d85ac0c982fea
FadyMilad22 <elfadyy2010@yahoo.com>
2025-06-20 21:08:31 +0300

    Payment feature editing

--- ClientNexus.Application/DTOs/StartSubscriptionPaymentRequestDTO.cs
+++ ClientNexus.Application/DTOs/StartSubscriptionPaymentRequestDTO.cs
@@ -10,25 +10,9 @@ namespace ClientNexus.Application.DTOs
     public class StartSubscriptionPaymentRequestDTO
     {
         [Required]
-        public int ServiceProviderId { get; set; }
-
-        [Required]
         public string SubscriptionType { get; set; } // "Monthly", "Quarterly", "Yearly"
 
         [Required]
         public string SubscriptionTier { get; set; } // "Normal", "Advanced"
-
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        [Required]
-        public string Phone { get; set; }
     }
 }
